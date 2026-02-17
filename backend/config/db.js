adaa2463196aac9df7c5180ea0a57479924fd6bc
Eishal Khan <133596850+eishalK@users.noreply.github.com>
2026-02-17 01:48:29 -0700

    Update MongoDB connection to include database name
    Specify database name for MongoDB connection and log active database.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: 'multi_ecommerce',
+        });
+       
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`Active Database: ${conn.connection.name}`);
         
     }
     catch (error) {
